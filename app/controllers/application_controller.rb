dc5081ab0b4fea4c40f913d49753959ee4931e7e
huynhduc96 <huynhduc96@gmail.com>
2018-07-24 15:30:38 +0700

    view profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    if I18n.available_locales
+           .map(&:to_s).include?(parsed_locale)
+      parsed_locale
+    end
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,7 @@ Bundler.require(*Rails.groups)
 module SampleApp
   class Application < Rails::Application
     config.load_defaults 5.1
+    I18n.available_locales = [:en, :vi]
+    I18n.default_locale = :en
   end
 end
